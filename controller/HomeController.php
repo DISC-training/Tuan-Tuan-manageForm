5f9c0b41f3ffd38b518f122a325cbc1a65fd3489
phuctrung0201 <mygalaxy2712@gmail.com>
2019-04-22 21:30:50 +0700

    reset data

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -7,7 +7,11 @@
                 'client'=>[
                                 ['name'=>'Tuan 1','birthday' => '21/04/2000'],
                                 ['name' => 'Tuan 2','birthday' => '21/03/2000']
-                            ]
+                ],
+                'form'=>[
+                    ['id'=> '1','name' => 'Apply for job','state' => 'waiting'],
+                    ['id'=> '2','name' => 'Ask about salary','state' => 'complete' ]
+                ]
                         ];
             $this->data = json_encode($data);
             echo $this->data;
